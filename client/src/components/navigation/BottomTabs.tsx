1e8bc5a953ac25a0acc52bb1e1b06c30810fc233
Kah Kit Zheng <kahkitzheng@gmail.com>
2022-02-21 14:51:41 +0100

    Fix: Include tab icons in the links of bottom navigation

--- client/src/components/navigation/BottomTabs.tsx
+++ client/src/components/navigation/BottomTabs.tsx
@@ -1,25 +1,31 @@
 import React from "react";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 import { HiHome } from "react-icons/hi";
 import { IoSearch } from "react-icons/io5";
 import { MdLibraryMusic } from "react-icons/md";
-import { Link } from "react-router-dom";
 
 const BottomTabs = () => {
   return (
     <BottomTabsWrapper>
-      <Tab>
-        <HiHome />
-        <TabName to={`/`}>Home</TabName>
-      </Tab>
-      <Tab>
-        <IoSearch />
-        <TabName to={`/search`}>Search</TabName>
-      </Tab>
-      <Tab>
-        <MdLibraryMusic />
-        <TabName to={`/library`}>Your Library</TabName>
-      </Tab>
+      <TabLink to={`/`}>
+        <TabIcon>
+          <HiHome />
+        </TabIcon>
+        <span>Home</span>
+      </TabLink>
+      <TabLink to={`/search`}>
+        <TabIcon>
+          <IoSearch />
+        </TabIcon>
+        <span>Search</span>
+      </TabLink>
+      <TabLink to={`/library`}>
+        <TabIcon>
+          <MdLibraryMusic />
+        </TabIcon>
+        <span>Your Library</span>
+      </TabLink>
     </BottomTabsWrapper>
   );
 };
@@ -28,32 +34,25 @@ const BottomTabsWrapper = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   align-items: center;
-  padding: 16px 0;
   background-color: #1c1f2f;
-  padding: 12px;
+  padding: 12px 24px;
 `;
 
-const Tab = styled.div`
+const TabLink = styled(Link)`
   display: flex;
   align-items: center;
   justify-content: center;
   flex-direction: column;
-  font-size: 1.25rem;
-
-  :first-of-type {
-    padding-left: 16px;
-  }
-
-  :last-of-type {
-    padding-right: 16px;
-  }
-`;
-
-const TabName = styled(Link)`
-  margin-top: 4px;
   font-size: 14px;
   color: currentColor;
   text-decoration: none;
+  margin: auto;
+  width: fit-content;
+`;
+
+const TabIcon = styled.div`
+  font-size: 1.25rem;
+  margin-bottom: 4px;
 `;
 
 export default BottomTabs;
