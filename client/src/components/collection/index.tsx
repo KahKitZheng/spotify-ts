8494af27aeae2223f290102899bd3185bbe3c6f2
Kah Kit Zheng <kahkitzheng@gmail.com>
2022-02-25 23:06:55 +0100

    Render fixed amount of cards in collection component depending on the viewport

--- client/src/components/collection/index.tsx
+++ client/src/components/collection/index.tsx
@@ -14,21 +14,45 @@ const Collection = (props: Props) => {
 };
 
 const CollectionGrid = styled.div<{ $colSize: number }>`
-  --colSize: ${({ $colSize }) => $colSize};
-
+  --colWidth: calc((100% / 2) - var(--gap));
+  --gap: 16px;
   display: grid;
   grid-template-columns: ${({ $colSize }) =>
-    $colSize && `repeat(${$colSize}, minmax(11rem, 1fr))`};
-  grid-gap: 16px;
+    $colSize && `repeat(${$colSize}, minmax(var(--colWidth), 1fr))`};
+  grid-gap: var(--gap);
   overflow-x: auto;
-  margin-left: -16px;
-  margin-right: -16px;
-  padding: 4px 16px 8px;
+  margin-left: calc(var(--gap) * -1);
+  margin-right: calc(var(--gap) * -1);
+  padding: 4px var(--gap) 8px;
   ${overflowNoScrollbar};
 
   /* Hide all items on the grid with a higher index than colSize */
   a:nth-child(1n + ${(props) => props.$colSize + 1}) {
     display: none;
+  }
+
+  @media (min-width: 560px) {
+    --colWidth: calc((100% / 3) - var(--gap));
+  }
+
+  @media (min-width: 680px) {
+    --colWidth: calc((100% / 4) - var(--gap));
+  }
+
+  @media (min-width: 950px) {
+    --colWidth: calc((100% / 5) - var(--gap));
+  }
+
+  @media (min-width: 1080px) {
+    --colWidth: calc((100% / 6) - var(--gap));
+  }
+
+  @media (min-width: 1300px) {
+    --colWidth: calc((100% / 7) - var(--gap));
+  }
+
+  @media (min-width: 14000px) {
+    --colWidth: calc((100% / 8) - var(--gap));
   }
 `;
 
