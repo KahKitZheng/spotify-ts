2c91c398ea65db356ce60e89c0991925ab413995
Kah Kit Zheng <kahkitzheng@gmail.com>
2022-02-26 00:15:29 +0100

    Make browse categories more responsive

--- client/src/pages/search/BrowseCategories.tsx
+++ client/src/pages/search/BrowseCategories.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import * as S from "../../styles/components/section";
 import { stringToHSL } from "../../utils";
 import { useAppSelector } from "../../app/hooks";
 import { selectCategories } from "../../slices/categoriesSlice";
@@ -8,7 +9,7 @@ const BrowseCategories = () => {
   const categories = useAppSelector(selectCategories);
 
   return (
-    <>
+    <CategoriesWrapper>
       <h3>Browse all</h3>
       <CategoriesGrid>
         {categories.items?.map((category) => (
@@ -18,25 +19,67 @@ const BrowseCategories = () => {
           </CategoryCard>
         ))}
       </CategoriesGrid>
-    </>
+    </CategoriesWrapper>
   );
 };
 
+const CategoriesWrapper = styled.div`
+  margin-top: 16px;
+`;
+
 const CategoriesGrid = styled.div`
+  --categoryRepeat: 2;
+  --categoryWidth: calc((100% / var(--categoryRepeat)) - var(--categoryGap));
+  --categoryGap: 16px;
   display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  grid-gap: 16px;
+  grid-template-columns: repeat(
+    var(--categoryRepeat),
+    minmax(var(--categoryGap), 1fr)
+  );
+
+  grid-template-rows: 100%;
+  grid-template-rows: auto;
+  grid-gap: var(--categoryGap);
   margin-top: 8px;
+
+  @media (min-width: 560px) {
+    --categoryRepeat: 3;
+  }
+
+  @media (min-width: 760px) {
+    --categoryRepeat: 4;
+  }
+
+  @media (min-width: 960px) {
+    --categoryRepeat: 5;
+  }
+
+  @media (min-width: 1160px) {
+    --categoryRepeat: 6;
+  }
+
+  @media (min-width: 1360px) {
+    --categoryRepeat: 7;
+  }
+
+  @media (min-width: 1560px) {
+    --categoryRepeat: 8;
+  }
 `;
 
 const CategoryCard = styled.div<{ bgColor: string }>`
-  border-radius: 4px;
+  border-radius: 8px;
   background-color: ${(props) => `${props.bgColor}`};
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.4);
   position: relative;
-  padding: 16px 12px;
-  height: 5rem;
+  padding: 16px;
+  height: 6rem;
   overflow: hidden;
+
+  @media (min-width: 560px) {
+    aspect-ratio: 1;
+    height: 100%;
+  }
 `;
 
 const CategoryName = styled.div`
@@ -47,13 +90,17 @@ const CategoryName = styled.div`
 `;
 
 const CategoryCover = styled.img`
-  height: 64px;
-  width: 64px;
+  aspect-ratio: 1;
+  max-height: 75%;
   position: absolute;
-  bottom: -4px;
-  right: -10px;
-  transform: rotate(20deg);
+  bottom: 0;
+  right: 0;
+  transform: rotate(20deg) translate(14%, -2%);
   box-shadow: -2px 4px 4px rgba(0, 0, 0, 0.4);
+
+  @media (min-width: 560px) {
+    max-height: 55%;
+  }
 `;
 
 export default BrowseCategories;
