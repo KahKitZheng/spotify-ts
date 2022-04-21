149f90f8d2987ff7436553d851009f3a1c88aecd
Kah Kit Zheng <kahkitzheng@gmail.com>
2022-04-21 15:54:28 +0200

    Show the current track in browser tab if a song is playing

--- client/src/components/AppRouter.tsx
+++ client/src/components/AppRouter.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import styled from "styled-components";
+import HelmetTitle from "./HelmetTitle/HelmetTitle";
 import MainViewWrapper from "./MainViewWrapper";
 import Playerbar from "./Playerbar";
 import NowPlayingMini from "./NowPlaying";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { BottomTabs, Sidebar } from "./Navigation";
 import { MEDIA } from "../styles/media";
+import { HelmetProvider } from "react-helmet-async";
+import { BottomTabs, Sidebar } from "./Navigation";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import HomePage from "../pages/home";
 import PageNotFound from "../pages/404";
@@ -17,36 +19,42 @@ import ArtistPage from "../pages/artist";
 import RecentTracksPage from "../pages/recent-tracks";
 import CategoryPage from "../pages/category";
 import GenrePage from "../pages/genre";
-import { TopArtistsPage, TopTracksPage } from "../pages/top-items/";
 import LikedSongsPage from "../pages/liked-songs";
+import { TopArtistsPage, TopTracksPage } from "../pages/top-items/";
 
 const AppRouter = () => (
   <BrowserRouter>
-    <Layout>
-      <Sidebar />
-      <MainViewWrapper>
-        <Routes>
-          <Route path="*" element={<PageNotFound />} />
-          <Route path="/" element={<HomePage />} />
-          <Route path="/search" element={<SearchPage />} />
-          <Route path="/library" element={<LibraryPage />} />
-          <Route path="/playlist/:id" element={<PlaylistPage />} />
-          <Route path="/album/:id" element={<AlbumPage />} />
-          <Route path="/artist/:id" element={<ArtistPage />} />
-          <Route path="/top-artists" element={<TopArtistsPage />} />
-          <Route path="/top-tracks" element={<TopTracksPage />} />
-          <Route path="/category/:id" element={<CategoryPage />} />
-          <Route path="/genre/:category/:artist" element={<GenrePage />} />
-          <Route path="/genre/recently-played" element={<RecentTracksPage />} />
-          <Route path="/liked-songs" element={<LikedSongsPage />} />
-        </Routes>
-      </MainViewWrapper>
-      <Bottom>
-        <NowPlayingMini />
-        <BottomTabs />
-      </Bottom>
-      <Playerbar />
-    </Layout>
+    <HelmetProvider>
+      <HelmetTitle />
+      <Layout>
+        <Sidebar />
+        <MainViewWrapper>
+          <Routes>
+            <Route path="*" element={<PageNotFound />} />
+            <Route path="/" element={<HomePage />} />
+            <Route path="/search" element={<SearchPage />} />
+            <Route path="/library" element={<LibraryPage />} />
+            <Route path="/liked-songs" element={<LikedSongsPage />} />
+            <Route path="/playlist/:id" element={<PlaylistPage />} />
+            <Route path="/album/:id" element={<AlbumPage />} />
+            <Route path="/artist/:id" element={<ArtistPage />} />
+            <Route path="/top-artists" element={<TopArtistsPage />} />
+            <Route path="/top-tracks" element={<TopTracksPage />} />
+            <Route path="/category/:id" element={<CategoryPage />} />
+            <Route path="/genre/:category/:artist" element={<GenrePage />} />
+            <Route
+              path="/genre/recently-played"
+              element={<RecentTracksPage />}
+            />
+          </Routes>
+        </MainViewWrapper>
+        <Bottom>
+          <NowPlayingMini />
+          <BottomTabs />
+        </Bottom>
+        <Playerbar />
+      </Layout>
+    </HelmetProvider>
   </BrowserRouter>
 );
 
